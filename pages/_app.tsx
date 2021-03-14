5d396f516aa17c684f12653cfca1e6610c3cf04a
duniandewon <duniandewon@gmail.com>
2021-03-15 06:19:33 +0700

    Setup redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app';
+import { wrapper } from '../store';
+
 import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
