53fb3c0184446c9287f8eef34428693b2d843a1e
duniandewon <duniandewon@gmail.com>
2021-03-19 06:32:58 +0700

    Install and setup react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Hydrate } from 'react-query/hydration';
 import { wrapper } from '../store';
 
 import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
